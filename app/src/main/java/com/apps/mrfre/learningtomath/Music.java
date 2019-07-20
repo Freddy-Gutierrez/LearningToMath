4bcf8e198fcb5023dd997d4d91b2817dd8b8d38a
Freddy Gutierrez <33851899+Freddy-Gutierrez@users.noreply.github.com>
2019-07-20 12:42:25 -0700

    -Free Play Mode added -Fixed minor bug with title for gameplay screen

--- app/src/main/java/com/apps/mrfre/learningtomath/Music.java
+++
@@ -1,8 +0,0 @@
-package com.apps.mrfre.learningtomath;
-
-public class Music {
-    public static boolean isPlaying = false;
-    public static boolean userPause = false;
-    public static boolean freePlay = false;
-    public static String freePlayValue = "Free Play : Off";
-}
